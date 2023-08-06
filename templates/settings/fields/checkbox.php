f2da825f855f21d8bc5b1c16f2c8d68537e05594
Sergiy Zakharchenko <zaharchenko.dev@gmail.com>
2023-08-06 18:44:13 +0100

    Possibility to remove emoji script and style from HTML

--- templates/settings/fields/checkbox.php
+++ templates/settings/fields/checkbox.php
@@ -3,6 +3,9 @@
  * @var string $id
  * @var string $name
  * @var bool $value
+ * @var bool $description
  * */
 ?>
-<input type="checkbox" id="<?php echo esc_attr( $id ) ?>" name="<?php echo esc_attr( $name ) ?>" value="1" <?php checked( $value, 1 ); ?> />
+<input type="checkbox" id="<?php echo esc_attr( $id ) ?>" name="<?php echo esc_attr( $name ) ?>" value="1" <?php
+    checked( $value, 1 ); ?> />
+<label for="<?php echo esc_attr( $id ) ?>"><?php echo esc_html( $description ) ?></label>
